3699e4fca03f44b90af1aab7bd6d8fbfb2b7032a
yybai <ybai@sfu.ca>
2018-02-16 14:28:23 -0800

    removed unuseful files

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Students = require('../models/students');
 
-// router.get('/', (req, res) => {
-//   Students.find({}, (err, students) => {
-//     res.render('index', { title: 'Welcome to Full-stack !', name: 'Michael', students })
-//   });
-// });
+
 
 
 router.get('/', (req, res) => {
--- routes/students.js
+++ routes/students.js
@@ -2,30 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Students = require('../models/students');
 
-//render add student page
-router.get('/add', (req, res, next) => {
-  res.render('add');
-})
-
-//render student detail page
-router.get('/:id', (req, res, next) => {
-  Students.findById(req.params.id, (err, student) =>{
-    res.render('detail', { student })
-  })
-})
-
-//handle add new student
-router.post('/add', (req, res, next) => {
-  const student = new Students(req.body)
-  student.save((err, doc)=>{
-    if (err){
-      res.send(err)
-    }
-    else{
-      res.redirect('/')
-    }
-  })
-})
 
 //handle update student
 router.post('/update/:id', (req, res, next) => {
@@ -39,16 +15,6 @@ router.post('/update/:id', (req, res, next) => {
   })
 })
 
-//handle delete student
-router.post('/delete', (req, res, next) => {
-  Students.remove({ _id: req.body.studentId }, err => {
-    if (err){
-      res.send(err)
-    }
-    else{
-      res.json({ result: 'success' })
-    }
-  })
-})
+
 
 module.exports = router;
